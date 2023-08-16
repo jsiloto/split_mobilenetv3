ee1f39d7eb0c09f2e2cb2d50aa78cba8881aee35
Juliano Siloto Assine <jsilotoa@uci.edu>
2023-08-16 23:00:57 +0000

    Fix weights

--- distillation_experiments.sh
+++ distillation_experiments.sh
@@ -8,7 +8,7 @@ PROJECT="baseline"
 for BETA in "0.01" "0.1" "1.0" "10.0"
 do
     python train.py --student configs/model/entropy_bottleneck_1.0_${BETA}.yaml \
-    --teacher configs/model/channel_bottleneck_1.0.yaml - $BASE_PARAMS
+    --teacher configs/model/channel_bottleneck_1.0.yaml $BASE_PARAMS
 done
 
 
