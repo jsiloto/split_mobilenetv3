ebfd77b90b9aba1a7cb786a4f0d449f42f3b6e14
Juliano Siloto Assine <jsilotoa@uci.edu>
2023-08-20 15:48:21 -0700

    New experiments

--- distillation_experiments.sh
+++ distillation_experiments.sh
@@ -3,7 +3,7 @@
 PROJECT="distillation"
 BASE_PARAMS="--hyper configs/hyper/default.yaml --project ${PROJECT} --clean --wandb"
 
-for BETA in "0.1" "1.0" "10.0"  "100.0"
+for BETA in "5.0" "50.0"
 do
     python train.py --student configs/model/entropy_bottleneck_1.0_${BETA}.yaml \
     --teacher configs/model/channel_bottleneck_1.0.yaml $BASE_PARAMS
