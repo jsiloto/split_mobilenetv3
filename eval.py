8dc243616020579fc35d8e616413003e4a113b0a
Juliano Siloto Assine <jsilotoa@uci.edu>
2023-08-28 02:09:05 +0000

    eval

---
+++ eval.py
@@ -0,0 +1,41 @@
+import argparse
+import json
+import os
+import shutil
+
+import torch
+import yaml
+from torch import nn
+
+from configs import get_config_from_args
+from dataset import get_dataset
+from distill_classifier import distill_classifier
+from eval_classifier import validate
+from models.models import get_model, resume_model
+from train_classifier import train_classifier
+from utils import mkdir_p
+
+def main():
+    parser = argparse.ArgumentParser(description='Train Model')
+    configs = get_config_from_args(parser)
+    with open(os.path.join(configs['checkpoint'], 'metadata.json'), "w") as f:
+        json.dump(configs, f)
+
+    print(configs)
+
+    d = get_dataset(configs['dataset'], configs['hyper']['batch_size'])
+    model = get_model(configs['model']['base_model'], configs['model']['model'], num_classes=d.num_classes)
+    # define loss function (criterion) and optimizer
+    val_criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = torch.optim.Adam(model.parameters(), configs['hyper']['lr'], weight_decay=1e-4)
+
+    # optionally resume from a checkpoint
+    checkpoint_path = configs['checkpoint']
+    model = resume_model(model, checkpoint_path, best=False)
+
+    for tier in range(len(model.encoder.betas)):
+        val_summary = validate(d.val_loader, d.val_loader_len, model, val_criterion, tier=tier)
+
+
+if __name__ == '__main__':
+    main()
--- models/split/gain_bottleneck.py
+++ models/split/gain_bottleneck.py
@@ -13,13 +13,12 @@ from models.split.split_model import SplitModel
 
 class MV3GainBottleneck(SplitModel):
     def __init__(self, base_model: MobileNetV3, bottleneck_ratio: float,
-                 split_position: int, bottleneck_position: int, beta: float, **kwargs):
+                 split_position: int, bottleneck_position: int, num_betas=6, max_beta=1.5, **kwargs):
         super().__init__()
         self.base_model = base_model
         self.split_position = split_position
         self.bottleneck_position = bottleneck_position
         self.num_classes = base_model.classifier[3].out_features
-        self.beta = beta
         bottleneck_channels = base_model.cfgs[self.bottleneck_position - 1][2]
         original_channels = base_model.cfgs[self.split_position - 1][2]
         encoder_layers_pre = list(base_model.features[:self.bottleneck_position])
@@ -30,11 +29,11 @@ class MV3GainBottleneck(SplitModel):
         # print("Original channels: ", original_channels)
         print("Bottleneck channels: ", bottleneck_channels)
 
-        self.encoder = MobileNetV3VanillaEncoder(layers_pre=encoder_layers_pre,
-                                                 layers_post=encoder_layers_post,
-                                                 bottleneck_channels=bottleneck_channels,
-                                                 bottleneck_ratio=bottleneck_ratio,
-                                                 beta=beta)
+        self.encoder = MobileNetV3GainEncoder(layers_pre=encoder_layers_pre,
+                                              layers_post=encoder_layers_post,
+                                              bottleneck_channels=bottleneck_channels,
+                                              bottleneck_ratio=bottleneck_ratio,
+                                              num_betas=num_betas, max_beta=max_beta)
         self.decoder = MobileNetV3Decoder(layers=decoder_layers,
                                           conv=base_model.conv,
                                           avgpool=base_model.avgpool,
@@ -42,27 +41,25 @@ class MV3GainBottleneck(SplitModel):
                                           original_channels=original_channels,
                                           bottleneck_ratio=-1)
 
-    def forward(self, x):
+    def forward(self, x, tier=0):
         output = {}
         pixels = x.shape[0] * x.shape[-1] * x.shape[-2] * x.shape[-3]
-        output = self.encoder(x)
-        output['compression_loss'] = -self.beta * output['bpp']
+        output = self.encoder(x, tier=tier)
         output['y_hat'] = self.decoder(output['y_hat'])
 
         return output
 
-    def compress(self, x):
+    def compress(self, x, tier=0):
         pixels = x.shape[-1] * x.shape[-2] * x.shape[-3]
-        output = self.encoder(x, compress=True)
+        output = self.encoder(x, compress=True, tier=tier)
         output['num_bytes'] = sum([len(s) for s in output['strings'][0]]) / len(output['strings'][0])
         output['bpp'] = output['num_bytes'] / pixels
         return output
 
 
-class MobileNetV3VanillaEncoder(nn.Module):
+class MobileNetV3GainEncoder(nn.Module):
     def __init__(self, layers_pre: List, layers_post: List,
-                 bottleneck_channels: int, bottleneck_ratio: float,
-                 beta: float):
+                 bottleneck_channels: int, bottleneck_ratio: float, num_betas=6, max_beta=1.5, **kwargs):
         super().__init__()
         self.layers_pre = nn.Sequential(*layers_pre)
         self.layers_post = nn.Sequential(*layers_post)
@@ -73,33 +70,36 @@ class MobileNetV3VanillaEncoder(nn.Module):
         entropy_bottleneck = EntropyBottleneck(self.bottleneck_channels, filters=(8, 8, 8, 8))
         self.codec = GainHyperLatentCodec(entropy_bottleneck=entropy_bottleneck)
         self.codec.entropy_bottleneck.update()
-        self.beta = beta
-        self.num_betas = 10
-        self.gain = torch.nn.init.uniform_(torch.randn( self.num_betas, self.bottleneck_channels, 1, 1)).to('cuda')
-        self.inv_gain = torch.nn.init.uniform_(torch.randn( self.num_betas, self.bottleneck_channels, 1, 1)).to('cuda')
+        self.num_betas = num_betas
 
+        self.gain = torch.nn.Parameter(torch.randn( self.num_betas, self.bottleneck_channels, 1, 1).to('cuda'))
+        self.inv_gain = torch.nn.Parameter(torch.randn( self.num_betas, self.bottleneck_channels, 1, 1).to('cuda'))
+        self.betas = torch.linspace(0.1, max_beta,  self.num_betas).to('cuda')
         # self.gain.requires_grad = False
         # self.inv_gain.requires_grad = False
 
-    def forward(self, x, compress=False, tier=5):
+    def forward(self, x, compress=False, tier=0):
         pixels = x.shape[0] * x.shape[-1] * x.shape[-2] * x.shape[-3]
         x = self.layers_pre(x)
 
-        betas = torch.linspace(0, 1.0,  self.num_betas).to('cuda')
+
         if self.training:
             self.tier = torch.randint(0, self.num_betas, (1,)).to('cuda')
+
 
         else:
             self.tier = tier
 
+        inv_gain = 1.0 / self.gain[self.tier]
+
         if compress:
-            x = self.codec.compress(x, self.gain[self.tier], self.inv_gain[self.tier])
+            x = self.codec.compress(x, self.gain[self.tier], inv_gain)
         else:
-            x = self.codec(x, self.gain[self.tier], self.inv_gain[self.tier])
+            x = self.codec(x, self.gain[self.tier], inv_gain)
             x['bpp'] = x['likelihoods']['z'].log2().sum() / pixels
-            x['compression_loss'] = -betas[self.tier] * x['bpp']
+            x['compression_loss'] = -self.betas[self.tier].item() * x['bpp']
             # print(x['bpp'], self.tier, x['compression_loss'])
-            print(x['compression_loss'])
+            # print(x['compression_loss'])
 
         x['y_hat'] = self.layers_post(x['params'])
 
